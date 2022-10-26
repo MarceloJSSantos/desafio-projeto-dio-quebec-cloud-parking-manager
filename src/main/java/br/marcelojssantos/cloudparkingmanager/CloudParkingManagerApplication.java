eba2a793c26cb7e192bb40029e4371fe328827f4
MarceloJSSantos <marcelojssantos2012@gmail.com>
2022-10-26 16:48:51 -0300

    Estrutura inicial

---
+++ src/main/java/br/marcelojssantos/cloudparkingmanager/CloudParkingManagerApplication.java
@@ -0,0 +1,13 @@
+package br.marcelojssantos.cloudparkingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudParkingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudParkingManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/marcelojssantos/cloudparkingmanager/CloudParkingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.marcelojssantos.cloudparkingmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudParkingManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
